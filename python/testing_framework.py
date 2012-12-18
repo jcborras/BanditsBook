d5ddd013bb126d2d57cdeb3babde235962931099
Juan Carlos Borrás <jcborras@gmail.com>
2012-12-19 00:25:03 +0200

    End of day 1

--- python/testing_framework.py
+++ python/testing_framework.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from csv import DictWriter
 from random import random, seed, shuffle
 from unittest import TestCase, main
 
@@ -25,23 +26,31 @@ def run_trial(alg, arms):
   alg.update(chosen, reward)
   return {'chosen_arm':chosen, 'reward':reward}
 
-def run_sumulation(alg,arms,hor):
+def run_simulation(alg,arms,hor):
   times = range(hor)
   x = [ run_trial(alg, arms) for i in times ]
   chosen_arm = [ z['chosen_arm'] for z in x ]
   rewards = [ z['reward'] for z in x ]
   cumulative_rewards = list(cumsum([ z['reward'] for z in x ]))
-  assert len(times)==len(chosen_arm)==len(rewards)
+  assert len(times)==len(chosen_arm)==len(rewards)==len(cumulative_rewards)
   return {'times':times, 'chosen_arm':chosen_arm, 'rewards':rewards, 'cumulative_rewards':cumulative_rewards}
+#  return [sim_nums, times, chosen_arms, rewards, cumulative_rewards]
 
-def f(d,n):
+def add_simulation_number(d,n):
   d['num_sim'] = len(d['times'])*[n]
   return d
   
 def run_montecarlo(alg, arms, n_sims, hor):
   alg.initialize(len(arms))
-  return [f(run_sumulation(alg, arms, hor),i) for i in range(n_sims)]
-          
+  return [add_simulation_number(run_simulation(alg, arms, hor),i) for i in range(n_sims)]
+
+def dict_from_simulations(l):
+  ll = list()
+  for d in l:
+    assert len(set([len(d[k]) for k in d.keys()]))==1
+    [ll.append(i) for i in map(dict, zip(*[[(k, v) for v in value] for k, value in d.items()]))]
+  return ll
+  
 def test_algorithm(algo, arms, num_sims, horizon):
   chosen_arms = [0.0 for i in range(num_sims * horizon)]
   rewards = [0.0 for i in range(num_sims * horizon)]
@@ -98,17 +107,42 @@ class EpsilonGreedyTest(TestCase):
     results2 = test_algorithm(algo2, self.arms, N_SIMS, HORIZON)
     self.assertEqual(results[0]['chosen_arm'], results2[2])
     self.assertEqual(results[0]['cumulative_rewards'], results2[4])
-    
+
+  def test_dict_from_simulations(self):
+    N_SIMS, HORIZON = 250, 100
+    algo = EpsilonGreedy(0.1, [], [])
+    algo.initialize(self.n_arms)
+    results = dict_from_simulations(run_montecarlo(algo, self.arms, N_SIMS, HORIZON))
+    self.assertEqual(len(results), N_SIMS*HORIZON)
+
   def test_standard(self):
+    N_SIMS, HORIZON = 1000, 250
     f = open(RESULTS_DIR+"epsilon_greedy_standard_results.csv", "w")
 
     for epsilon in [0.1, 0.2, 0.3, 0.4, 0.5]:
       algo = EpsilonGreedy(epsilon, [], [])
       algo.initialize(self.n_arms)
-      results = test_algorithm(algo, self.arms, 5000, 250)
+      results = test_algorithm(algo, self.arms, N_SIMS, HORIZON)
       for i in range(len(results[0])):
         f.write(str(epsilon) + ",")
         f.write(",".join([str(results[j][i]) for j in range(len(results))]) + "\n")
+    f.close()
+
+  def results_to_file(r, f):
+    fieldnames = ['num_sim', 'times', 'chosen_arm', 'rewards','cumulative_rewards']
+    
+  def test_standard2(self):
+    N_SIMS, HORIZON = 10, 25
+    f = open(RESULTS_DIR+"epsilon_greedy_standard_results2.csv",'w')
+    #dw = DictWriter(f, fieldnames = ['num_sim', 'times', 'chosen_arm', 'rewards','cumulative_rewards'])
+    #dw.writeheader()
+    for epsilon in [0.1, 0.2, 0.3, 0.4, 0.5]:
+      algo = EpsilonGreedy(epsilon, [], [])
+      algo.initialize(self.n_arms)
+      #results = dict_from_simulations(run_montecarlo(algo, self.arms, N_SIMS, HORIZON))
+      results = run_montecarlo(algo, self.arms, N_SIMS, HORIZON)
+      print results
+      #dw.writerows(results)
     f.close()
 
   def test_annealing(self):
